e844e0eec1a14b976cea08971bcc8016d94beb19
roshnikatrak <rkatrak-adefowora@ucsb.edu>
2022-04-08 11:27:52 -0700

    begin almond yield function

---
+++ R/almond_yield.R
@@ -0,0 +1,12 @@
+#' Almond Yield Function
+#' 
+#' This function computes almond yield (tons/acre)
+#' @param precip January precipitation (mm)
+#' @param temp February minimum temperature (C)
+#' 
+almond_yield = function(precip, temp){
+  
+  anomaly = -0.15*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28
+  
+  return(anomaly)
+}
