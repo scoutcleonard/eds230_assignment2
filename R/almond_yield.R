6c89d3625db16fa2e065b4edc62ea2141afe6202
roshnikatrak <rkatrak-adefowora@ucsb.edu>
2022-04-08 11:50:42 -0700

    fixed function

--- R/almond_yield.R
+++ R/almond_yield.R
@@ -6,7 +6,7 @@
 #' 
 almond_yield = function(precip, temp){
   
-  anomaly = -0.15*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28
+  anomaly = -0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28
   
   return(anomaly)
 }
