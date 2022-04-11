8bec5513df274d6e28a82cee1fc70928d4725ba1
scoutcleonard <88209342+scoutcleonard@users.noreply.github.com>
2022-04-11 07:30:45 -0700

    updated the almod yield function to include params vars

--- R/almond_yield.R
+++ R/almond_yield.R
@@ -4,9 +4,19 @@
 #' @param precip January precipitation (mm)
 #' @param temp February minimum temperature (C)
 #' 
-almond_yield = function(precip, temp){
+almond_yield = function(precip, 
+                        temp, 
+                        temp_param_1 = -0.015, 
+                        temp_param_2 = -0.0046, 
+                        precip_param_1 = -0.07, 
+                        precip_param_2 = 0.0043, 
+                        constant = 0.28){
   
-  anomaly = -0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28
+  anomaly = temp_param_1 * temp +
+    temp_param_2 * temp^2 +
+    precip_param_1 * precip +
+    precip_param_2 * precip^2 +
+    constant
   
   return(anomaly)
 }
