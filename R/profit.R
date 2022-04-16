4ca226be4f7f67a1e7d4bf0e297efad67bac4c9a
Nikole Vannest <50924274+nrvannest@users.noreply.github.com>
2022-04-16 12:02:03 -0700

    created data hist_price.txt, added code to assignment3 and updated profit.R

--- R/profit.R
+++ R/profit.R
@@ -1,4 +1,13 @@
 #' Almond Profit Function
 #'
 #' This function calculates the profit generated from almond yield
-#' @param 
+#' @param yield List of yearly yields from almond_yield function (tons)
+#' @param price Average price in ($/ton)
+#' @return yearly profit ($)
+#' 
+#' Equation
+
+profit <- function(yield, price){
+  money <- yield * price
+  return(money)
+}
